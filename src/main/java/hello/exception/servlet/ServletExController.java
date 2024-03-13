fd422a1c6bcb61749aac536f18d194ff6dfe55d4
sdsr <dlxodnr1212@gmail.com>
2024-03-13 21:17:46 +0900

    Handling Exception, Error Page

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -21,6 +21,11 @@ public class ServletExController {
         response.sendError(404, "404 error!");
     }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 error!");
+    }
+
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500, "500 error!");
